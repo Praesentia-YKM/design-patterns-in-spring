5a25f93a6072a86fafbf5dff88cc47c1ba42466d
Praesentia-YKM <ykm8864@naver.com>
2025-09-23 08:47:04 +0900

    feat. enum을 통한 싱글톤패턴 구현 장단점 주석 적용

--- src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/Settings5.java
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/Settings5.java
@@ -2,6 +2,9 @@ package me.whiteship.designpatterns._01_creational_patterns._01_singleton;
 
 /**
  * Enum을 사용해서 싱글톤 만들기
+ * 상속을 쓰지 못한다.
+ * 직렬화 역직렬화 시 객체 동일성보장
+ * lazy initialization이 안된다
  */
 public enum Settings5 {
 
